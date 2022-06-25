80fc5c8e2e485d0ad500e78f779b7d22d46da73b
Wai-Yin Kwan <wykhuh@users.noreply.github.com>
2022-06-25 05:38:05 -0700

    clean up code to show wikidata links

--- src/lib/components/wikidata_link.svelte
+++ src/lib/components/wikidata_link.svelte
@@ -1,15 +1,18 @@
 <script>
+  import { notEmpty } from '$lib/common/utils';
+
   export let value;
+
+  let nestedValue = value['data_value']['value'];
+  let wikiTypes = ['wikibase-item', 'wikibase-property', 'wikibase-lexeme'];
 </script>
 
-{#if value['data_type'] == 'commonsMedia'}
-  <a href={value['data_value']['value']['url']}>Wikimedia Commons link&rarr;</a>
-{:else if value['data_type'] == 'wikibase-item'}
-  <a href={value['data_value']['value']['url']}>Wikidata link&rarr;</a>
-{:else if value['data_type'] == 'wikibase-property'}
-  <a href={value['data_value']['value']['url']}>Wikidata link&rarr;</a>
-{:else if value['data_type'] == 'wikibase-lexeme'}
-  <a href={value['data_value']['value']['url']}>Wikidata link&rarr;</a>
+{#if notEmpty(nestedValue['url'])}
+  {#if value['data_type'] == 'commonsMedia'}
+    <a href={nestedValue['url']}>Wikimedia Commons link&rarr;</a>
+  {:else if wikiTypes.includes(value['data_type'])}
+    <a href={nestedValue['url']}>Wikidata link&rarr;</a>
+  {/if}
 {/if}
 
 <style>
