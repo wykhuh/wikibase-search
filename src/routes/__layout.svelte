f17f5be08bd46e3199ba8cbe9bee90c743076401
Wai-Yin Kwan <wykhuh@users.noreply.github.com>
2022-06-29 01:40:46 -0700

    add nav menu

---
+++ src/routes/__layout.svelte
@@ -0,0 +1,20 @@
+<nav class="navbar" aria-label="main navigation">
+  <div class="navbar-menu  is-active">
+    <a href="/" class="navbar-item">Home</a>
+    <a href="/linked_data" class="navbar-item">Linked Data</a>
+  </div>
+</nav>
+
+<section class="main">
+  <slot />
+</section>
+
+<style>
+  nav {
+    border-bottom: 1px solid #999;
+  }
+
+  .main {
+    margin-top: 2rem;
+  }
+</style>
