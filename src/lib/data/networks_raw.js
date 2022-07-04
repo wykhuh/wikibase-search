4d78e9956bb99ca24ad47b7982f0869f178829e4
Wai-Yin Kwan <wykhuh@users.noreply.github.com>
2022-07-04 07:43:56 -0700

    add downloaded data from wikidata

---
+++ src/lib/data/networks_raw.js
@@ -0,0 +1,250 @@
+export let martha = [
+  {
+    prop: 'http://www.wikidata.org/entity/P1809',
+    propLabel: 'choreographer',
+    selectedItem: 'http://www.wikidata.org/entity/Q487604',
+    selectedItemLabel: 'Martha Graham',
+    itemR: 'http://www.wikidata.org/entity/Q23035425',
+    itemRLabel: 'Primitive Mysteries'
+  },
+  {
+    prop: 'http://www.wikidata.org/entity/P802',
+    propLabel: 'student',
+    selectedItem: 'http://www.wikidata.org/entity/Q487604',
+    selectedItemLabel: 'Martha Graham',
+    itemR: 'http://www.wikidata.org/entity/Q268760',
+    itemRLabel: 'Ruth St. Denis'
+  },
+  {
+    prop: 'http://www.wikidata.org/entity/P1809',
+    propLabel: 'choreographer',
+    selectedItem: 'http://www.wikidata.org/entity/Q487604',
+    selectedItemLabel: 'Martha Graham',
+    itemR: 'http://www.wikidata.org/entity/Q21998489',
+    itemRLabel: 'Errand into the Maze'
+  },
+  {
+    prop: 'http://www.wikidata.org/entity/P802',
+    propLabel: 'student',
+    selectedItem: 'http://www.wikidata.org/entity/Q487604',
+    selectedItemLabel: 'Martha Graham',
+    itemF: 'http://www.wikidata.org/entity/Q2850889',
+    itemFLabel: 'Anne-Marie Reynaud'
+  },
+  {
+    prop: 'http://www.wikidata.org/entity/P1066',
+    propLabel: 'student of',
+    selectedItem: 'http://www.wikidata.org/entity/Q487604',
+    selectedItemLabel: 'Martha Graham',
+    itemR: 'http://www.wikidata.org/entity/Q94284201',
+    itemRLabel: 'Faith Reyher Jackson'
+  },
+  {
+    prop: 'http://www.wikidata.org/entity/P802',
+    propLabel: 'student',
+    selectedItem: 'http://www.wikidata.org/entity/Q487604',
+    selectedItemLabel: 'Martha Graham',
+    itemF: 'http://www.wikidata.org/entity/Q452238',
+    itemFLabel: 'Yvonne Rainer'
+  },
+  {
+    prop: 'http://www.wikidata.org/entity/P1066',
+    propLabel: 'student of',
+    selectedItem: 'http://www.wikidata.org/entity/Q268760',
+    selectedItemLabel: 'Ruth St. Denis',
+    itemR: 'http://www.wikidata.org/entity/Q11672008',
+    itemRLabel: 'Seiko Takata'
+  },
+  {
+    prop: 'http://www.wikidata.org/entity/P802',
+    propLabel: 'student',
+    selectedItem: 'http://www.wikidata.org/entity/Q487604',
+    selectedItemLabel: 'Martha Graham',
+    itemF: 'http://www.wikidata.org/entity/Q2899920',
+    itemFLabel: 'Bessie Schonberg'
+  },
+  {
+    prop: 'http://www.wikidata.org/entity/P1809',
+    propLabel: 'choreographer',
+    selectedItem: 'http://www.wikidata.org/entity/Q487604',
+    selectedItemLabel: 'Martha Graham',
+    itemR: 'http://www.wikidata.org/entity/Q110913074',
+    itemRLabel: 'Clytemnestra'
+  },
+  {
+    prop: 'http://www.wikidata.org/entity/P802',
+    propLabel: 'student',
+    selectedItem: 'http://www.wikidata.org/entity/Q268760',
+    selectedItemLabel: 'Ruth St. Denis',
+    itemF: 'http://www.wikidata.org/entity/Q487604',
+    itemFLabel: 'Martha Graham'
+  },
+  {
+    prop: 'http://www.wikidata.org/entity/P1066',
+    propLabel: 'student of',
+    selectedItem: 'http://www.wikidata.org/entity/Q487604',
+    selectedItemLabel: 'Martha Graham',
+    itemR: 'http://www.wikidata.org/entity/Q108602902',
+    itemRLabel: 'Gertrude Lippincott'
+  },
+  {
+    prop: 'http://www.wikidata.org/entity/P802',
+    propLabel: 'student',
+    selectedItem: 'http://www.wikidata.org/entity/Q268760',
+    selectedItemLabel: 'Ruth St. Denis',
+    itemR: 'http://www.wikidata.org/entity/Q3293012',
+    itemRLabel: 'Marietta Bonfanti'
+  },
+  {
+    prop: 'http://www.wikidata.org/entity/P1066',
+    propLabel: 'student of',
+    selectedItem: 'http://www.wikidata.org/entity/Q487604',
+    selectedItemLabel: 'Martha Graham',
+    itemF: 'http://www.wikidata.org/entity/Q3517048',
+    itemFLabel: 'Ted Shawn'
+  },
+  {
+    prop: 'http://www.wikidata.org/entity/P1066',
+    propLabel: 'student of',
+    selectedItem: 'http://www.wikidata.org/entity/Q487604',
+    selectedItemLabel: 'Martha Graham',
+    itemR: 'http://www.wikidata.org/entity/Q105946732',
+    itemRLabel: 'James Starbuck'
+  },
+  {
+    prop: 'http://www.wikidata.org/entity/P1066',
+    propLabel: 'student of',
+    selectedItem: 'http://www.wikidata.org/entity/Q487604',
+    selectedItemLabel: 'Martha Graham',
+    itemR: 'http://www.wikidata.org/entity/Q97577998',
+    itemRLabel: 'John R. Hawkins'
+  },
+  {
+    prop: 'http://www.wikidata.org/entity/P1066',
+    propLabel: 'student of',
+    selectedItem: 'http://www.wikidata.org/entity/Q487604',
+    selectedItemLabel: 'Martha Graham',
+    itemF: 'http://www.wikidata.org/entity/Q268760',
+    itemFLabel: 'Ruth St. Denis'
+  },
+  {
+    prop: 'http://www.wikidata.org/entity/P1809',
+    propLabel: 'choreographer',
+    selectedItem: 'http://www.wikidata.org/entity/Q487604',
+    selectedItemLabel: 'Martha Graham',
+    itemR: 'http://www.wikidata.org/entity/Q3216714',
+    itemRLabel: 'Lamentation'
+  },
+  {
+    prop: 'http://www.wikidata.org/entity/P1066',
+    propLabel: 'student of',
+    selectedItem: 'http://www.wikidata.org/entity/Q487604',
+    selectedItemLabel: 'Martha Graham',
+    itemR: 'http://www.wikidata.org/entity/Q2850889',
+    itemRLabel: 'Anne-Marie Reynaud'
+  },
+  {
+    prop: 'http://www.wikidata.org/entity/P1809',
+    propLabel: 'choreographer',
+    selectedItem: 'http://www.wikidata.org/entity/Q487604',
+    selectedItemLabel: 'Martha Graham',
+    itemR: 'http://www.wikidata.org/entity/Q620638',
+    itemRLabel: 'Appalachian Spring'
+  },
+  {
+    prop: 'http://www.wikidata.org/entity/P1066',
+    propLabel: 'student of',
+    selectedItem: 'http://www.wikidata.org/entity/Q487604',
+    selectedItemLabel: 'Martha Graham',
+    itemR: 'http://www.wikidata.org/entity/Q452238',
+    itemRLabel: 'Yvonne Rainer'
+  },
+  {
+    prop: 'http://www.wikidata.org/entity/P1809',
+    propLabel: 'choreographer',
+    selectedItem: 'http://www.wikidata.org/entity/Q487604',
+    selectedItemLabel: 'Martha Graham',
+    itemR: 'http://www.wikidata.org/entity/Q25217452',
+    itemRLabel: 'Judith'
+  },
+  {
+    prop: 'http://www.wikidata.org/entity/P1809',
+    propLabel: 'choreographer',
+    selectedItem: 'http://www.wikidata.org/entity/Q487604',
+    selectedItemLabel: 'Martha Graham',
+    itemR: 'http://www.wikidata.org/entity/Q25217465',
+    itemRLabel: 'Maple Leaf Rag'
+  },
+  {
+    prop: 'http://www.wikidata.org/entity/P1066',
+    propLabel: 'student of',
+    selectedItem: 'http://www.wikidata.org/entity/Q487604',
+    selectedItemLabel: 'Martha Graham',
+    itemR: 'http://www.wikidata.org/entity/Q111744184',
+    itemRLabel: 'Edith Stephen'
+  },
+  {
+    prop: 'http://www.wikidata.org/entity/P1066',
+    propLabel: 'student of',
+    selectedItem: 'http://www.wikidata.org/entity/Q487604',
+    selectedItemLabel: 'Martha Graham',
+    itemR: 'http://www.wikidata.org/entity/Q2899920',
+    itemRLabel: 'Bessie Schonberg'
+  },
+  {
+    prop: 'http://www.wikidata.org/entity/P1066',
+    propLabel: 'student of',
+    selectedItem: 'http://www.wikidata.org/entity/Q487604',
+    selectedItemLabel: 'Martha Graham',
+    itemR: 'http://www.wikidata.org/entity/Q111824230',
+    itemRLabel: 'Muriel Sharon'
+  },
+  {
+    prop: 'http://www.wikidata.org/entity/P1066',
+    propLabel: 'student of',
+    selectedItem: 'http://www.wikidata.org/entity/Q268760',
+    selectedItemLabel: 'Ruth St. Denis',
+    itemR: 'http://www.wikidata.org/entity/Q487604',
+    itemRLabel: 'Martha Graham'
+  },
+  {
+    prop: 'http://www.wikidata.org/entity/P802',
+    propLabel: 'student',
+    selectedItem: 'http://www.wikidata.org/entity/Q268760',
+    selectedItemLabel: 'Ruth St. Denis',
+    itemF: 'http://www.wikidata.org/entity/Q11672008',
+    itemFLabel: 'Seiko Takata'
+  },
+  {
+    prop: 'http://www.wikidata.org/entity/P1066',
+    propLabel: 'student of',
+    selectedItem: 'http://www.wikidata.org/entity/Q268760',
+    selectedItemLabel: 'Ruth St. Denis',
+    itemF: 'http://www.wikidata.org/entity/Q3293012',
+    itemFLabel: 'Marietta Bonfanti'
+  },
+  {
+    prop: 'http://www.wikidata.org/entity/P1809',
+    propLabel: 'choreographer',
+    selectedItem: 'http://www.wikidata.org/entity/Q487604',
+    selectedItemLabel: 'Martha Graham',
+    itemR: 'http://www.wikidata.org/entity/Q57502753',
+    itemRLabel: 'Q57502753'
+  },
+  {
+    prop: 'http://www.wikidata.org/entity/P1066',
+    propLabel: 'student of',
+    selectedItem: 'http://www.wikidata.org/entity/Q487604',
+    selectedItemLabel: 'Martha Graham',
+    itemR: 'http://www.wikidata.org/entity/Q80743697',
+    itemRLabel: 'Silvianna Goldsmith'
+  },
+  {
+    prop: 'http://www.wikidata.org/entity/P1809',
+    propLabel: 'choreographer',
+    selectedItem: 'http://www.wikidata.org/entity/Q487604',
+    selectedItemLabel: 'Martha Graham',
+    itemR: 'http://www.wikidata.org/entity/Q21998396',
+    itemRLabel: 'Cave of the Heart'
+  }
+];
