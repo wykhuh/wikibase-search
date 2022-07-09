b779732a0a8b41173189758644cc804742c961f0
Wai-Yin Kwan <wykhuh@users.noreply.github.com>
2022-07-09 16:52:27 -0700

    update roundNumber to handle really small or large numbers

--- src/lib/common/utils.js
+++ src/lib/common/utils.js
@@ -8,8 +8,8 @@ export function notEmpty(value) {
 
 export function roundNumber(num, scale) {
   // round number to certain number of decimal places
-  // https://stackoverflow.com/a/12830454
-  return +(Math.round(num + 'e+' + scale) + 'e-' + scale);
+  // https://stackoverflow.com/a/19722641
+  return +(Math.round(+(num.toFixed(scale) + 'e+' + scale)) + 'e-' + scale);
 }
 
 export function secondsToHms(seconds) {
