7523728334af1bf7a41577f6df9cd35f291a7e93
Wai-Yin Kwan <wykhuh@users.noreply.github.com>
2022-07-09 10:39:50 -0700

    cleanup code on /import_wikidata

--- src/routes/import_wikidata.svelte
+++ src/routes/import_wikidata.svelte
@@ -4,10 +4,13 @@
 
   let records = [];
   let ca_table = 'ca_entities';
+  let loading = false;
 
   onMount(async () => {
     if (ca_table === 'ca_entities') {
+      loading = true;
       records = await getEntitiesWithoutWikidataId();
+      loading = false;
     } else {
       throw new Error(`${ca_table} not implemented`);
     }
@@ -16,8 +19,13 @@
 
 <h1 class="title is-1">Import Wikidata Info</h1>
 
-<ol>
-  {#each records as record (record['id'])}
-    <li><a href={`import_wikidata/${record['id']}`}>{record['Display name']}</a></li>
-  {/each}
-</ol>
+{#if loading}
+  <p>Loading...</p>
+{:else}
+  <p>These Collective Access records do not have corresponding Wikidata records.</p>
+  <ol>
+    {#each records as record (record['id'])}
+      <li><a href={`import_wikidata/${record['id']}`}>{record['Display name']}</a></li>
+    {/each}
+  </ol>
+{/if}
