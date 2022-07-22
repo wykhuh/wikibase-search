b90d2902cb980bea61cbffe70ac94618e9dd25b1
Wai-Yin Kwan <wykhuh@users.noreply.github.com>
2022-07-22 08:39:56 -0700

    delete unused route

--- src/routes/linked_data_graph_demo.svelte
+++
@@ -1,9 +0,0 @@
-<script>
-  import { martha } from '$lib/data/networks_raw.js';
-  import NetworkGraph from '$lib/components/network_vis.svelte';
-  import { formatNetworkGraphDataForVisJs } from '$lib/common/wiki_queries';
-
-  let data = formatNetworkGraphDataForVisJs(martha);
-</script>
-
-<NetworkGraph {data} />
