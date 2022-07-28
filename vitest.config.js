e017f6e36ed87b1d37bf1372925868165f0641c9
Wai-Yin Kwan <wykhuh@users.noreply.github.com>
2022-07-27 18:32:52 -0700

    add dsv plugin to vitest

--- vitest.config.js
+++ vitest.config.js
@@ -2,11 +2,14 @@
 // https://stackoverflow.com/questions/70801691/vitetest-failed-to-resolve-import-lib-stores-store
 import { defineConfig } from 'vitest/config';
 import path from 'path';
+import dsv from '@rollup/plugin-dsv';
 
 export default defineConfig({
   resolve: {
     alias: {
       $lib: path.resolve('./src/lib')
     }
-  }
+  },
+
+  plugins: [dsv()]
 });
