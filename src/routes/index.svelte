0a58664eb8754a94a50445a4725367ca150b5e17
Wai-Yin Kwan <wykhuh@users.noreply.github.com>
2023-10-10 23:28:46 -0700

    move title and attribution to home page

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,147 +1,43 @@
 <script>
-  import AutoComplete from 'simple-svelte-autocomplete';
-  import { onMount } from 'svelte';
-  import Claim from '$lib/components/claim.svelte';
-  import ItemBasicInfo from '$lib/components/item_basic_info.svelte';
-  import { searchKeyword, fetchWikidataItem } from '$lib/common/wiki_queries';
-
-  let testIds = {
-    Q5: 'human',
-    Q30: 'United States', // place
-    Q487604: 'Martha Graham', // dancing digital
-    Q16973731: 'Dianne McIntyre', // dancing digital
-    Q753828: 'Essex', // place
-    Q76: 'Barack Obama', // has lexeme
-    Q28425: 'Chiroptera', // has video, sound, images
-    Q111420520: 'Karl Hirsch'
-  };
-  let testId = null;
-
-  let searchItem = '';
-  let currentItem = null;
-  let currentId = testId;
-  let currentLabel = testIds[testId];
-  let loading = false;
-  let languageCodesDisplay = [];
-  let targetWiki = 'wikidata';
-
-  // ====================
-  // display record
-  // ====================
-
-  function setlanguageCodesDisplay(item) {
-    let languages = item['languages'];
-    let tmp = Object.keys(languages);
-    // ensure 'en' is first language shown
-    if (languages['en']) {
-      let en_idx = tmp.indexOf('en');
-      tmp = ['en', ...tmp.slice(0, en_idx), ...tmp.slice(en_idx + 1)];
-    }
-
-    languageCodesDisplay = tmp;
-  }
-
-  function displayItem(item) {
-    setlanguageCodesDisplay(item);
-  }
-
-  function resetSearch() {
-    currentItem = null;
-    currentId = null;
-    currentLabel = null;
-    searchItem = '';
-  }
-
-  // ====================
-  // autocomplete
-  // ====================
-
-  async function loadOptions(keyword) {
-    if (keyword.length > 1) {
-      let json = await searchKeyword(keyword, targetWiki);
-      return json;
-    } else {
-      return [];
-    }
-  }
-
-  async function handleSelect(selectedOption) {
-    if (!selectedOption) return;
-    loading = true;
-    currentItem = null;
-    currentId = selectedOption['id'];
-    currentLabel = selectedOption['label'];
-    currentItem = await fetchWikidataItem(currentId, targetWiki);
-    displayItem(currentItem);
-    loading = false;
-  }
-
-  // ====================
-  // life cycle
-  // ====================
-
-  onMount(async () => {
-    if (currentId) {
-      loading = true;
-      currentItem = await fetchWikidataItem(currentId, targetWiki);
-      displayItem(currentItem);
-      loading = false;
-    }
-  });
+  import ADHC_logo from '$lib/images/ADHClogo.png';
+  import ADance_logo from '$lib/images/CapstoneALogo-Dept.TheatreDance.jpg';
+  import NEH_logo from '$lib/images/NEH-Preferred-Seal.jpg';
+  import UT_logo from '$lib/images/RGB_formal_Fine_Arts.svg?raw';
+  import whirl_logo from '$lib/images/whirlLogo.jpg';
+  import SvgIcon from '../lib/components/svg_icon.svelte';
 </script>
 
-<h1 class="title is-1">Search Wikidata.org</h1>
+<h1 class="title is-1">
+  Dancing Digital/No Boundaries Archive Project - Demo Wikidata Integration
+</h1>
 
-<div class="columns">
-  <div class="column is-four-fifths">
-    <AutoComplete
-      searchFunction={loadOptions}
-      delay="200"
-      onChange={handleSelect}
-      labelFieldName="search_label"
-      placeholder="Search keyword"
-      hideArrow={true}
-      showClear={false}
-      localFiltering={false}
-      lowercaseKeywords={false}
-      bind:selectedItem={searchItem}
-    />
-  </div>
-  <div class="column">
-    <button class="button is-small" on:click={resetSearch}>Reset</button>
-  </div>
-</div>
+<p class="attribution">
+  Co-directors Gesel Mason (University of Texas at Austin) and Rebecca Salzer (University of
+  Alabama). Built by Whirl-i-Gig: Wai-Yin Kwan, Eve Perry, Seth Kaufman
+</p>
 
-{#if loading}
-  <h2 class="title is-2">Loading...</h2>
-{/if}
-
-{#if currentItem}
-  <h2 class="title is-2">{currentLabel} ({currentId})</h2>
-
-  <ItemBasicInfo item={currentItem} languageCodes={languageCodesDisplay} />
-
-  <h3 class="title is-3">Statements</h3>
-  {#each Object.values(currentItem['statements']) as claimProperty}
-    {#each claimProperty as claim (claim.id)}
-      <Claim {claim} />
-    {/each}
-  {/each}
-
-  <h3 class="title is-3">Identifiers</h3>
-  {#each Object.values(currentItem['identifiers']) as claimProperty}
-    {#each claimProperty as claim (claim.id)}
-      <Claim {claim} />
-    {/each}
-  {/each}
-{/if}
+<SvgIcon svg={UT_logo} class="logo-svg" />
+<img class="logo" src={ADHC_logo} alt="University of Alabama Digital Humanities Center logo" />
+<img class="logo" src={ADance_logo} alt="University of Alabama Arts and Science logo" />
+<img class="logo" src={NEH_logo} alt="National Endownment for the Humanities logo" />
+<img class="logo" src={whirl_logo} alt="Whirl-i-Gig logo" />
 
 <style>
-  /* use css to hide the arrow in <Autocomplete> since hideArrow does not work
-  with bulma
-  https://github.com/pstanoev/simple-svelte-autocomplete/issues/150
-  */
-  :global(.select:not(.is-multiple):not(.is-loading)::after) {
-    border: 0;
+  .logo {
+    max-height: 70px;
+    display: inline-block;
+    margin: 5px 10px;
+  }
+
+  :global(.logo-svg) {
+    max-height: 70px;
+    background-color: white;
+    max-width: 400px;
+    margin: 5px 10px;
+  }
+
+  .attribution {
+    margin-bottom: 2rem;
+    font-size: 1.5rem;
   }
 </style>
